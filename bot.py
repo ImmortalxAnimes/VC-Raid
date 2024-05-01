5be5a5e6194547ed44f8a62f24c8e6e480452fae
Immortal Sar <157793850+ImmortalxAnimes@users.noreply.github.com>
2024-05-01 09:22:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,10 +2,10 @@ import os
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
 
-API_ID = os.environ.get("API_ID")
-API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-ASSISTANT_ID = os.environ.get("ASSISTANT_ID")
+API_ID = os.environ.get("17271772")
+API_HASH = os.environ.get("897542330c90728e4e7fef57f42f9c79")
+BOT_TOKEN = os.environ.get("6931069312:AAER2u5Zw_KhM-ZxZPwBBJyWf146nzWGIZA")
+ASSISTANT_ID = os.environ.get("BQEHi9wAvRq1uU1SK7y_E7mxlk5EeG1S31Y4Sm6X9mtqeD1levwVaOe3mTpilDv184g6EkjEWRd0cbPBS94sTj89hxPZawWWCRgit_BlrOeF-Xx6WdocQ2Ayok_yBZunQuk9cSD1TvTKGOR2Ov17sXihFUYFjBA3hJ9mpBov8fGyAWCgQiRS6OMafulbYIemXZUK1mPCQ69xygzavQ0gDZQtBFXFyOpIf7-8HN2pXNag5Wn4KEp-LTLfH95hFfWaT7WLA1dxOfAFYlzxUAJ-bUuUYYtkzPm3gEyvW4P56KI3Ls3ZqtAd2MpOYDCTAe7JsRGzZnJzlJnylAAIV7B_gwurHIXd6wAAAAB_Ez2fAA")
 
 app = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 pytgcalls = PyTgCalls(app)
